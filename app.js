2d7381e29ef0532f456d55bf58a716b27ef72530
DevineJW <Johnwdevine85@gmail.com>
2021-05-14 12:13:26 -0500

    9.1

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length)
 console.log(profileDataArgs)
 
-const printProfileData = (profileDataArr) => {
-  console.log(profileDataArr)
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i])
+  }
 }
+console.log('=============')
+
+const printProfileData = profileDataArr => {
+profileDataArr.forEach((profileItem) => {
+  console.log(profileItem);
+})
+}
+
+
 printProfileData(profileDataArgs)
+
+// const addNums = function(numOne, numTwo) {
+//   return numOne + numTwo
+// }
+// const addNums = (numOne, numTwo) => {
+//   return numOne + numTwo
+// }
